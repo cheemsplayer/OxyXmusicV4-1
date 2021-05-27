573aeb1217227ae716159081bae1e0f527f8acec
OxyNotOp <jaydippadhiyar587@gmail.com>
2021-05-27 09:41:59 +0000

    Auto Fixes: Code formatted

--- OxyXmusic/modules/gcast.py
+++ OxyXmusic/modules/gcast.py
@@ -1,12 +1,14 @@
-from pyrogram import Client, filters
-from pyrogram.errors import UserAlreadyParticipant
 import asyncio
+
+from pyrogram import Client, filters
+
 from OxyXmusic.config import SUDO_USERS
+
 
 @Client.on_message(filters.command(["gcast"]))
 async def bye(client, message):
-    sent=0
-    failed=0
+    sent = 0
+    failed = 0
     if message.from_user.id in SUDO_USERS:
         if not message.reply_to_message.text:
             await message.reply("Reply to any text message to gcast sir")
@@ -15,8 +17,10 @@ async def bye(client, message):
         for dialog in client.iter_dialogs():
             try:
                 await client.send_message(dialog.chat.id, msg)
-                sent = sent+1
+                sent = sent + 1
             except:
-                failed=failed+1
+                failed = failed + 1
             await asyncio.sleep(3)
-        await message.reply_text(f"Gcasted message to {sent} chats. Failed {failed} chats.")
+        await message.reply_text(
+            f"Gcasted message to {sent} chats. Failed {failed} chats."
+        )
