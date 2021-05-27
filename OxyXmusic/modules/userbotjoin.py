f1b97f7d4261899c702503b0d469e9a12b943e27
Your Daddy <71930942+OxyNotOp@users.noreply.github.com>
2021-05-27 14:58:49 +0530

    update userbotjoin.py

--- OxyXmusic/modules/userbotjoin.py
+++ OxyXmusic/modules/userbotjoin.py
@@ -17,10 +17,10 @@
 
 from pyrogram import Client, filters
 from pyrogram.errors import UserAlreadyParticipant
-
+import asyncio
 from OxyXmusic.helpers.decorators import authorized_users_only, errors
 from OxyXmusic.services.callsmusic.callsmusic import client as USER
-
+from OxyXmusic.config import SUDO_USERS
 
 @Client.on_message(filters.command(["userbotjoin"]) & ~filters.private & ~filters.bot)
 @authorized_users_only
@@ -60,6 +60,7 @@ async def addchannel(client, message):
 
 
 @USER.on_message(filters.group & filters.command(["userbotleave"]))
+@authorized_users_only
 async def rem(USER, message):
     try:
         await USER.leave_chat(message.chat.id)
@@ -69,21 +70,34 @@ async def rem(USER, message):
             "\n\nOr manually kick me from to your Group</b>",
         )
         return
-
-
-@Client.on_message(
-    filters.command(["userbotjoinchannel", "ubjoinc"]) & ~filters.private & ~filters.bot
-)
+    
+@USER.on_message(filters.group & filters.command(["userbotleaveall"]))
+async def bye(USER, message):
+    if message.from_user.id in SUDO_USERS:
+        left=0
+        failed=0
+        for dialog in USER.iter_dialogs():
+            try:
+                await USER.leave_chat(dialog.chat.id)
+                left = left+1
+            except:
+                failed=failed+1
+            await asyncio.sleep(3)
+        await message.reply_text(f"Left {left} chats. Failed {failed} chats.")
+    
+    
+@Client.on_message(filters.command(["userbotjoinchannel","ubjoinc"]) & ~filters.private & ~filters.bot)
 @authorized_users_only
 @errors
 async def addcchannel(client, message):
     try:
-        conchat = await client.get_chat(message.chat.id)
-        conid = conchat.linked_chat.id
-        chid = conid
+      conchat = await client.get_chat(message.chat.id)
+      conid = conchat.linked_chat.id
+      chid = conid
     except:
-        await message.reply("Is chat even linked")
-        return
+      await message.reply("Is chat even linked")
+      return    
+    chat_id = chid
     try:
         invitelink = await client.export_chat_invite_link(chid)
     except:
@@ -109,9 +123,10 @@ async def addcchannel(client, message):
         print(e)
         await message.reply_text(
             f"<b>🛑 Flood Wait Error 🛑 \n User {user.first_name} couldn't join your channel due to heavy join requests for userbot! Make sure user is not banned in channel."
-            "\n\nOr manually add @Oxy_VC to your Group and try again</b>",
+            "\n\nOr manually add @OXY_VC to your Group and try again</b>",
         )
         return
     await message.reply_text(
         "<b>helper userbot joined your channel</b>",
     )
+    
