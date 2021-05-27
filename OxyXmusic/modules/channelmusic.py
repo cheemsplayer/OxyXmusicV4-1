ae351fc3a7a28e8bb8cf160ac2ef37543eec61d7
Your Daddy <71930942+OxyNotOp@users.noreply.github.com>
2021-05-27 18:47:29 +0530

    update channelmusic.py

--- OxyXmusic/modules/channelmusic.py
+++ OxyXmusic/modules/channelmusic.py
@@ -15,25 +15,42 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 
+import json
 import os
 from os import path
+from typing import Callable
 
+import aiofiles
+import aiohttp
+import ffmpeg
 import requests
 import wget
+from PIL import Image, ImageDraw, ImageFont
 from pyrogram import Client, filters
 from pyrogram.errors import UserAlreadyParticipant
+from pyrogram.types import Voice
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
+from Python_ARQ import ARQ
 from youtube_search import YoutubeSearch
-
+from OxyXmusic.modules.play import generate_cover
+from OxyXmusic.modules.play import arq
+from OxyXmusic.modules.play import cb_admin_check
+from OxyXmusic.modules.play import transcode
+from OxyXmusic.modules.play import convert_seconds
+from OxyXmusic.modules.play import time_to_seconds
+from OxyXmusic.modules.play import changeImageSize
 from OxyXmusic.config import BOT_NAME as bn
 from OxyXmusic.config import DURATION_LIMIT
 from OxyXmusic.config import UPDATES_CHANNEL as updateschannel
 from OxyXmusic.config import que
-from OxyXmusic.helpers.admins import get_administrators
-from OxyXmusic.helpers.decorators import authorized_users_only
+from OxyXmusic.function.admins import admins as a
 from OxyXmusic.helpers.errors import DurationLimitError
+from OxyXmusic.helpers.decorators import errors
+from OxyXmusic.helpers.admins import get_administrators
+from OxyXmusic.helpers.channelmusic import get_chat_id
+from OxyXmusic.helpers.decorators import authorized_users_only
+from OxyXmusic.helpers.filters import command, other_filters
 from OxyXmusic.helpers.gets import get_file_name
-from OxyXmusic.modules.play import arq, cb_admin_check, generate_cover
 from OxyXmusic.services.callsmusic import callsmusic, queues
 from OxyXmusic.services.callsmusic.callsmusic import client as USER
 from OxyXmusic.services.converter.converter import convert
@@ -42,16 +59,15 @@ from OxyXmusic.services.downloaders import youtube
 chat_id = None
 
 
-@Client.on_message(
-    filters.command(["channelplaylist", "cplaylist"]) & filters.group & ~filters.edited
-)
+
+@Client.on_message(filters.command(["channelplaylist","cplaylist"]) & filters.group & ~filters.edited)
 async def playlist(client, message):
     try:
-        lel = await client.get_chat(message.chat.id)
-        lol = lel.linked_chat.id
+      lel = await client.get_chat(message.chat.id)
+      lol = lel.linked_chat.id
     except:
-        message.reply("Is this cat even linked?")
-        return
+      message.reply("Is this cat even linked?")
+      return
     global que
     queue = que.get(lol)
     if not queue:
@@ -116,17 +132,15 @@ def r_ply(type_):
     return mar
 
 
-@Client.on_message(
-    filters.command(["channelcurrent", "ccurrent"]) & filters.group & ~filters.edited
-)
+@Client.on_message(filters.command(["channelcurrent","ccurrent"]) & filters.group & ~filters.edited)
 async def ee(client, message):
     try:
-        lel = await client.get_chat(message.chat.id)
-        lol = lel.linked_chat.id
-        conv = lel.linked_chat
+      lel = await client.get_chat(message.chat.id)
+      lol = lel.linked_chat.id
+      conv = lel.linked_chat
     except:
-        await message.reply("Is chat even linked")
-        return
+      await message.reply("Is chat even linked")
+      return
     queue = que.get(lol)
     stats = updated_stats(conv, queue)
     if stats:
@@ -135,19 +149,17 @@ async def ee(client, message):
         await message.reply("No VC instances running in this chat")
 
 
-@Client.on_message(
-    filters.command(["channelplayer", "cplayer"]) & filters.group & ~filters.edited
-)
+@Client.on_message(filters.command(["channelplayer","cplayer"]) & filters.group & ~filters.edited)
 @authorized_users_only
 async def settings(client, message):
     playing = None
     try:
-        lel = await client.get_chat(message.chat.id)
-        lol = lel.linked_chat.id
-        conv = lel.linked_chat
+      lel = await client.get_chat(message.chat.id)
+      lol = lel.linked_chat.id
+      conv = lel.linked_chat
     except:
-        await message.reply("Is chat even linked")
-        return
+      await message.reply("Is chat even linked")
+      return
     queue = que.get(lol)
     stats = updated_stats(conv, queue)
     if stats:
@@ -164,11 +176,11 @@ async def settings(client, message):
 async def p_cb(b, cb):
     global que
     try:
-        lel = await client.get_chat(cb.message.chat.id)
-        lol = lel.linked_chat.id
-        conv = lel.linked_chat
+      lel = await client.get_chat(cb.message.chat.id)
+      lol = lel.linked_chat.id
+      conv = lel.linked_chat
     except:
-        return
+      return    
     que.get(lol)
     type_ = cb.matches[0].group(1)
     cb.message.chat.id
@@ -210,17 +222,18 @@ async def m_cb(b, cb):
     ):
         chet_id = int(chat.title[13:])
     else:
-        try:
-            lel = await b.get_chat(cb.message.chat.id)
-            lol = lel.linked_chat.id
-            conv = lel.linked_chat
-            chet_id = lol
-        except:
-            return
+      try:
+        lel = await b.get_chat(cb.message.chat.id)
+        lol = lel.linked_chat.id
+        conv = lel.linked_chat
+        chet_id = lol
+      except:
+        return
     qeue = que.get(chet_id)
     type_ = cb.matches[0].group(1)
     cb.message.chat.id
     m_chat = cb.message.chat
+    
 
     the_data = cb.message.reply_markup.inline_keyboard[1][0].callback_data
     if type_ == "cpause":
@@ -232,7 +245,9 @@ async def m_cb(b, cb):
             callsmusic.pytgcalls.pause_stream(chet_id)
 
             await cb.answer("Music Paused!")
-            await cb.message.edit(updated_stats(conv, qeue), reply_markup=r_ply("play"))
+            await cb.message.edit(
+                updated_stats(conv, qeue), reply_markup=r_ply("play")
+            )
 
     elif type_ == "cplay":
         if (chet_id not in callsmusic.pytgcalls.active_calls) or (
@@ -343,26 +358,24 @@ async def m_cb(b, cb):
             await cb.answer("Chat is not connected!", show_alert=True)
 
 
-@Client.on_message(
-    filters.command(["channelplay", "cplay"]) & filters.group & ~filters.edited
-)
+@Client.on_message(filters.command(["channelplay","cplay"])  & filters.group & ~filters.edited)
 @authorized_users_only
 async def play(_, message: Message):
     global que
     lel = await message.reply("🔄 **Processing**")
 
     try:
-        conchat = await _.get_chat(message.chat.id)
-        conv = conchat.linked_chat
-        conid = conchat.linked_chat.id
-        chid = conid
+      conchat = await _.get_chat(message.chat.id)
+      conv = conchat.linked_chat
+      conid = conchat.linked_chat.id
+      chid = conid
     except:
-        await message.reply("Is chat even linked")
-        return
+      await message.reply("Is chat even linked")
+      return
     try:
-        administrators = await get_administrators(conv)
+      administrators = await get_administrators(conv)
     except:
-        await message.reply("Am I admin of Channel")
+      await message.reply("Am I admin of Channel")
     try:
         user = await USER.get_me()
     except:
@@ -379,6 +392,7 @@ async def play(_, message: Message):
                     await lel.edit(
                         "<b>Remember to add helper to your channel</b>",
                     )
+                    pass
 
                 try:
                     invitelink = await _.export_chat_invite_link(chid)
@@ -411,6 +425,7 @@ async def play(_, message: Message):
         )
         return
     message.from_user.id
+    text_links = None
     message.from_user.first_name
     await lel.edit("🔎 **Finding**")
     message.from_user.id
@@ -425,12 +440,14 @@ async def play(_, message: Message):
             entities = message.reply_to_message.entities + entities
         elif message.reply_to_message.caption_entities:
             entities = message.reply_to_message.entities + entities
-        urls = [entity for entity in entities if entity.type == "url"]
-        text_links = [entity for entity in entities if entity.type == "text_link"]
+        urls = [entity for entity in entities if entity.type == 'url']
+        text_links = [
+            entity for entity in entities if entity.type == 'text_link'
+        ]
     else:
-        urls = None
+        urls=None
     if text_links:
-        urls = True
+        urls = True    
     audio = (
         (message.reply_to_message.audio or message.reply_to_message.voice)
         if message.reply_to_message
@@ -487,7 +504,7 @@ async def play(_, message: Message):
             print(str(e))
             return
         dlurl = url
-        dlurl = dlurl.replace("youtube", "youtubepp")
+        dlurl=dlurl.replace("youtube","youtubepp")
         keyboard = InlineKeyboardMarkup(
             [
                 [
@@ -503,7 +520,7 @@ async def play(_, message: Message):
         )
         requested_by = message.from_user.first_name
         await generate_cover(requested_by, title, views, duration, thumbnail)
-        file_path = await convert(youtube.download(url))
+        file_path = await convert(youtube.download(url))        
     else:
         query = ""
         for i in message.command[1:]:
@@ -532,7 +549,7 @@ async def play(_, message: Message):
             return
 
         dlurl = url
-        dlurl = dlurl.replace("youtube", "youtubepp")
+        dlurl=dlurl.replace("youtube","youtubepp")
         keyboard = InlineKeyboardMarkup(
             [
                 [
@@ -586,26 +603,24 @@ async def play(_, message: Message):
         return await lel.delete()
 
 
-@Client.on_message(
-    filters.command(["channeldplay", "cdplay"]) & filters.group & ~filters.edited
-)
+@Client.on_message(filters.command(["channeldplay","cdplay"]) & filters.group & ~filters.edited)
 @authorized_users_only
 async def deezer(client: Client, message_: Message):
     global que
     lel = await message_.reply("🔄 **Processing**")
 
     try:
-        conchat = await client.get_chat(message_.chat.id)
-        conid = conchat.linked_chat.id
-        conv = conchat.linked_chat
-        chid = conid
+      conchat = await client.get_chat(message_.chat.id)
+      conid = conchat.linked_chat.id
+      conv = conchat.linked_chat
+      chid = conid
     except:
-        await message_.reply("Is chat even linked")
-        return
+      await message_.reply("Is chat even linked")
+      return
     try:
-        administrators = await get_administrators(conv)
+      administrators = await get_administrators(conv)
     except:
-        await message.reply("Am I admin of Channel")
+      await message.reply("Am I admin of Channel") 
     try:
         user = await USER.get_me()
     except:
@@ -622,6 +637,7 @@ async def deezer(client: Client, message_: Message):
                     await lel.edit(
                         "<b>Remember to add helper to your channel</b>",
                     )
+                    pass
                 try:
                     invitelink = await client.export_chat_invite_link(chid)
                 except:
@@ -656,11 +672,11 @@ async def deezer(client: Client, message_: Message):
 
     text = message_.text.split(" ", 1)
     queryy = text[1]
-    query = queryy
+    query=queryy
     res = lel
     await res.edit(f"Searching 👀👀👀 for `{queryy}` on deezer")
     try:
-        songs = await arq.deezer(query, 1)
+        songs = await arq.deezer(query,1)
         if not songs.ok:
             await message_.reply_text(songs.result)
             return
@@ -719,25 +735,23 @@ async def deezer(client: Client, message_: Message):
     os.remove("final.png")
 
 
-@Client.on_message(
-    filters.command(["channelsplay", "csplay"]) & filters.group & ~filters.edited
-)
+@Client.on_message(filters.command(["channelsplay","csplay"]) & filters.group & ~filters.edited)
 @authorized_users_only
 async def jiosaavn(client: Client, message_: Message):
     global que
     lel = await message_.reply("🔄 **Processing**")
     try:
-        conchat = await client.get_chat(message_.chat.id)
-        conid = conchat.linked_chat.id
-        conv = conchat.linked_chat
-        chid = conid
+      conchat = await client.get_chat(message_.chat.id)
+      conid = conchat.linked_chat.id
+      conv = conchat.linked_chat
+      chid = conid
     except:
-        await message_.reply("Is chat even linked")
-        return
+      await message_.reply("Is chat even linked")
+      return
     try:
-        administrators = await get_administrators(conv)
+      administrators = await get_administrators(conv)
     except:
-        await message.reply("Am I admin of Channel")
+      await message.reply("Am I admin of Channel")
     try:
         user = await USER.get_me()
     except:
@@ -754,6 +768,7 @@ async def jiosaavn(client: Client, message_: Message):
                     await lel.edit(
                         "<b>Remember to add helper to your channel</b>",
                     )
+                    pass
                 try:
                     invitelink = await client.export_chat_invite_link(chid)
                 except:
@@ -774,7 +789,7 @@ async def jiosaavn(client: Client, message_: Message):
                     # print(e)
                     await lel.edit(
                         f"<b>🔴 Flood Wait Error 🔴 \nUser {user.first_name} couldn't join your channel due to heavy requests for userbot! Make sure user is not banned in group."
-                        "\n\nOr manually add @Music_op_bot to your Group and try again</b>",
+                        "\n\nOr manually add @Oxy_VC to your Group and try again</b>",
                     )
     try:
         await USER.get_chat(chid)
