edeb67165349578fe4be1f02cd698aaf0d842799
Your Daddy <71930942+OxyNotOp@users.noreply.github.com>
2021-05-27 21:57:57 +0530

    update private.py

--- OxyXmusic/modules/private.py
+++ OxyXmusic/modules/private.py
@@ -1,7 +1,5 @@
 # OxyXmusic (Telegram bot project )
-
-# Copyright (C) 2021  Inukaasith
-
+# Copyright (C) 2021  OxyNotOp
 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as
@@ -124,4 +122,3 @@ async def ghelp(_, message: Message):
             ]
         ),
     )
-
