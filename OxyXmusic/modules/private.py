276ddcebb214b3e6411559032fd743691c096285
Your Daddy <71930942+OxyNotOp@users.noreply.github.com>
2021-05-27 15:24:12 +0530

    update private.py

--- OxyXmusic/modules/private.py
+++ OxyXmusic/modules/private.py
@@ -15,48 +15,35 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 import logging
-
-from pyrogram import Client, filters
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
-
-from OxyXmusic.config import BOT_USERNAME, PROJECT_NAME, SUPPORT_GROUP, UPDATES_CHANNEL
 from OxyXmusic.modules.msg import Messages as tr
-
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
+from OxyXmusic.config import SOURCE_CODE,ASSISTANT_NAME,PROJECT_NAME,SUPPORT_GROUP,UPDATES_CHANNEL,BOT_USERNAME
 logging.basicConfig(level=logging.INFO)
 
-
-@Client.on_message(filters.private & filters.incoming & filters.command(["start"]))
+@Client.on_message(filters.private & filters.incoming & filters.command(['start']))
 def _start(client, message):
-    client.send_message(
-        message.chat.id,
+    client.send_message(message.chat.id,
         text=tr.START_MSG.format(message.from_user.first_name, message.from_user.id),
         parse_mode="markdown",
         reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
-                        "➕ α∂∂ мε тσ үσυя gяσυρ 🙋‍♀️",
-                        url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
-                    )
-                ],
+                        "➕ α∂∂ мε тσ үσυя gяσυρ 🙋‍♀️", url=f"https://t.me/{BOT_USERNAME}?startgroup=true")],
                 [
                     InlineKeyboardButton(
-                        "❤️ αвσυт σxү ❤️", url=f"https://t.me/{UPDATES_CHANNEL}"
-                    ),
+                        "❤️ αвσυт σxү ❤️", url=f"https://t.me/{UPDATES_CHANNEL}"), 
                     InlineKeyboardButton(
-                        "💬 sυρρσят", url=f"https://t.me/{SUPPORT_GROUP}"
-                    ),
-                ],
-                [
+                        "💬 sυρρσят", url=f"https://t.me/{SUPPORT_GROUP}")
+                ],[
                     InlineKeyboardButton(
-                        "🔥 мү σωηεя 🔥", url=f"https://t.me/FallenAngel_xD"
-                    )
-                ],
+                        "🔥 мү σωηεя 🔥", url=f"https://t.me/FallenAngel_xD")
+                ]
             ]
         ),
-        reply_to_message_id=message.message_id,
-    )
-
+        reply_to_message_id=message.message_id
+        )
 
 @Client.on_message(filters.command("start") & ~filters.private & ~filters.channel)
 async def gstart(_, message: Message):
@@ -66,7 +53,7 @@ async def gstart(_, message: Message):
             [
                 [
                     InlineKeyboardButton(
-                        "💬 sυρρσят cнαт", url=f"https://t.me/{SUPPORT_GROUP}"
+                        "🔥 мү σωηεя 🔥", url=f"https://t.me/FallenAngel_xD"
                     )
                 ]
             ]
@@ -74,73 +61,52 @@ async def gstart(_, message: Message):
     )
 
 
-@Client.on_message(filters.private & filters.incoming & filters.command(["help"]))
+@Client.on_message(filters.private & filters.incoming & filters.command(['help']))
 def _help(client, message):
-    client.send_message(
-        chat_id=message.chat.id,
-        text=tr.HELP_MSG[1],
+    client.send_message(chat_id = message.chat.id,
+        text = tr.HELP_MSG[1],
         parse_mode="markdown",
         disable_web_page_preview=True,
         disable_notification=True,
-        reply_markup=InlineKeyboardMarkup(map(1)),
-        reply_to_message_id=message.message_id,
+        reply_markup = InlineKeyboardMarkup(map(1)),
+        reply_to_message_id = message.message_id
     )
 
-
-help_callback_filter = filters.create(
-    lambda _, __, query: query.data.startswith("help+")
-)
-
+help_callback_filter = filters.create(lambda _, __, query: query.data.startswith('help+'))
 
 @Client.on_callback_query(help_callback_filter)
 def help_answer(client, callback_query):
     chat_id = callback_query.from_user.id
+    disable_web_page_preview=True
     message_id = callback_query.message.message_id
-    msg = int(callback_query.data.split("+")[1])
-    client.edit_message_text(
-        chat_id=chat_id,
-        message_id=message_id,
-        text=tr.HELP_MSG[msg],
-        reply_markup=InlineKeyboardMarkup(map(msg)),
+    msg = int(callback_query.data.split('+')[1])
+    client.edit_message_text(chat_id=chat_id,    message_id=message_id,
+        text=tr.HELP_MSG[msg],    reply_markup=InlineKeyboardMarkup(map(msg))
     )
 
 
 def map(pos):
-    if pos == 1:
-        button = [[InlineKeyboardButton(text="▶️", callback_data="help+2")]]
-    elif pos == len(tr.HELP_MSG) - 1:
+    if(pos==1):
+        button = [
+            [InlineKeyboardButton(text = '▶️', callback_data = "help+2")]
+        ]
+    elif(pos==len(tr.HELP_MSG)-1):
         url = f"https://t.me/{SUPPORT_GROUP}"
         button = [
-            [
-                InlineKeyboardButton(
-                    "➕ α∂∂ мε тσ үσυя gяσυρ 🙋‍♀️",
-                    url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
-                )
-            ],
-            [
-                InlineKeyboardButton(
-                    text="❤️ αвσυт σxү ❤️", url=f"https://t.me/{UPDATES_CHANNEL}"
-                ),
-                InlineKeyboardButton(
-                    text="💬 sυρρσят", url=f"https://t.me/{SUPPORT_GROUP}"
-                ),
-            ],
-            [
-                InlineKeyboardButton(
-                    text="🔥 мү σωηεя 🔥", url=f"https://t.me/FallenAngel_xD"
-                )
-            ],
-            [InlineKeyboardButton(text="◀️", callback_data=f"help+{pos-1}")],
+            [InlineKeyboardButton("➕ Add me to your Group 🙋‍♀️", url=f"https://t.me/{BOT_USERNAME}?startgroup=true")],
+            [InlineKeyboardButton(text = ,❤️ αвσυт σxү ❤️' url=f"https://t.me/{UPDATES_CHANNEL}"),
+             InlineKeyboardButton(text = '💬 sυρρσят', url=f"https://t.me/{SUPPORT_GROUP}")],
+            [InlineKeyboardButton(text = ,🔥 мү σωηεя 🔥' url=f"https://t.me/FallenAngel_xD")],
+            [InlineKeyboardButton(text = '◀️', callback_data = f"help+{pos-1}")]
         ]
     else:
         button = [
             [
-                InlineKeyboardButton(text="◀️", callback_data=f"help+{pos-1}"),
-                InlineKeyboardButton(text="▶️", callback_data=f"help+{pos+1}"),
+                InlineKeyboardButton(text = '◀️', callback_data = f"help+{pos-1}"),
+                InlineKeyboardButton(text = '▶️', callback_data = f"help+{pos+1}")
             ],
         ]
     return button
-
 
 @Client.on_message(filters.command("help") & ~filters.private & ~filters.channel)
 async def ghelp(_, message: Message):
@@ -150,8 +116,7 @@ async def ghelp(_, message: Message):
             [
                 [
                     InlineKeyboardButton(
-                        "🟡 cℓιcк нεяε ғσя нεℓρ 🟡",
-                        url=f"https://t.me/{BOT_USERNAME}?start",
+                        "🟡 cℓιcк нεяε ғσя нεℓρ 🟡", url=f"https://t.me/{BOT_USERNAME}?start"
                     )
                 ]
             ]
